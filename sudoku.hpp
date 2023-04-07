850836f739b0859708d2dac4ff7ab07c7967c24c
MaciejGd <128997250+MaciejGd@users.noreply.github.com>
2023-04-07 19:06:11 +0200

    Update sudoku.hpp

--- sudoku.hpp
+++ sudoku.hpp
@@ -2,4 +2,3 @@ bool possible(int y,int x, int n, int board[9][9]);
 bool solve(int (&board)[9][9]);
 void print_board(int board[9][9]);
 void opening_example_from_file(int (&board)[9][9]);
-void uzupelnij(int (&board)[9][9]);
