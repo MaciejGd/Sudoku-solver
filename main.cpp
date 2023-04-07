95725a4c4c59a2e9b6fac9b77a556af28408f23f
MaciejGd <128997250+MaciejGd@users.noreply.github.com>
2023-04-07 19:05:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,6 @@
 
 using namespace std;
 
-bool possible(int y,int x, int n, int board[9][9]);
-bool solve(int (&board)[9][9]);
-void print_board(int board[9][9]);
-void opening_example_from_file(int (&board)[9][9]);
-void uzupelnij(int (&board)[9][9]);
-
 int main(){
 
     //grid of numbers
